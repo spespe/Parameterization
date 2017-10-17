8b49192022ae628076d63aeb17aa87acae2d5886
Pietro.Speri <speri.pietro@gmail.com>
2017-10-17 10:46:27 -0700

    Added the extractor

--- src/main/scala/type_parameterization.scala
+++ src/main/scala/type_parameterization.scala
@@ -55,3 +55,12 @@ object Queue {
       new QueueImpl(leading, x :: trailing)
   }
 }
+
+object extractor{
+  def apply(user: String, domain:String): String = user + "@" + domain
+
+  def unapply(str:String):Option[(String,String)]= {
+    val parts=str.split("@")
+    if (parts.length==2) Some(parts(0), parts(1)) else None
+  }
+}
