4a1b3b4cc035fbd253f05d1c8abdf188ca7275c6
Pietro.Speri <speri.pietro@gmail.com>
2017-11-02 08:55:46 -0700

    Added the object queues implementing the 3 traits extending the abstract class

---
+++ src/main/scala/Queues.scala
@@ -0,0 +1,41 @@
+import scala.collection.mutable.ArrayBuffer
+/**
+  * Created by Pietro.Speri on 02/11/2017.
+  */
+object Queues {
+
+  abstract class IntQueue {
+    def get(): Int
+
+    def put(x: Int)
+  }
+
+  class BasicIntQueue extends IntQueue {
+    val buf = new ArrayBuffer[Int]
+
+    def get(): Int = buf.remove(0)
+
+    def put(x: Int) = {
+      buf += x
+    }
+  }
+
+  trait Incrementing extends IntQueue {
+    abstract override def put(x: Int) = {
+      super.put(x + 1)
+    }
+  }
+
+  trait Doubling extends IntQueue {
+    abstract override def put(x: Int) = {
+      super.put(x * 2)
+    }
+  }
+
+  trait Filtering extends IntQueue {
+    abstract override def put(x: Int) = {
+      if (x >= 0) super.put(x)
+    }
+  }
+
+}
