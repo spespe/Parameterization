d1dca5f9ccb5a437bc612c2295ffdced5a4fc66b
Pietro.Speri <speri.pietro@gmail.com>
2017-11-06 09:52:58 -0800

    Adding the fromXML and save

--- src/main/scala/XMLInfo.scala
+++ src/main/scala/XMLInfo.scala
@@ -1,8 +1,19 @@
+import scala.
 /**
   * Created by Pietro.Speri on 06/11/2017.
   */
 object XMLInfo {
   def main(args:Array[String])={
+    val inf = new Inf {
+      val description = args(0).toString
+      val date = args(1).toString
+      val domain = args(2).toString
+      val seconds = args(3).toInt
+      val orders = args(4).toString
+    }
+
+    val node = inf.toXML
+    scala.xml.XML.save(args(5).toString+"information.xml", node)
 
   }
 
@@ -26,6 +37,12 @@ object XMLInfo {
   }
 
 
-
+  def fromXML(node: scala.xml.Node) = new Inf {
+    val description = (node \ "description").text
+    val date = (node \ "date").text
+    val domain = (node \ "domain").text
+    val seconds = (node \ "seconds").text
+    val orders = (node \ "orders").text
+  }
 
 }
