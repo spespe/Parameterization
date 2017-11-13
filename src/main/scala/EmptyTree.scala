22e3e7db85b3fc65c5561f981b77e97ce9a02a8c
Pietro.Speri <speri.pietro@gmail.com>
2017-11-13 12:58:44 -0800

    Added changes in class Branch

--- src/main/scala/EmptyTree.scala
+++ src/main/scala/EmptyTree.scala
@@ -1,3 +1,5 @@
+import sun.reflect.generics.tree.Tree
+
 /**
   * Created by Pietro.Speri on 13/11/2017.
   */
@@ -15,5 +17,16 @@ trait Tree[+T]{
   def elem: T
   def left:Tree[T]
   def rigth:Tree[T]
+}
+
+class Branch[+T](val elem:T, val left:Tree[T], val right:Tree[T]) extends Tree[T] {
+  override def equals(other: Any):Boolean = other match {
+    case that: Branch[_] => (that canEqual this ) && this.elem == that.elem && this.left == that.left && this.right == that.right
+    case _ => false
+  }
+
+  def canEqual(other: Any) = other.isInstanceOf[Branch[_]]
+
+  override def hashCode(): Int = (elem, left, right).##
 }
 
