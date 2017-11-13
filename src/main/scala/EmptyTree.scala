a5b16ead3b7f4ac488aa717cffc3a786c8c05f3e
Pietro.Speri <speri.pietro@gmail.com>
2017-11-13 13:02:49 -0800

    Changed right method (vowel)

--- src/main/scala/EmptyTree.scala
+++ src/main/scala/EmptyTree.scala
@@ -9,17 +9,18 @@ object EmptyTree extends Tree[Nothing]{
 
   override def left: Tree[Nothing] =  throw new NoSuchElementException
 
-  override def rigth: Tree[Nothing] = throw new NoSuchElementException
+  override def right: Tree[Nothing] = throw new NoSuchElementException
 
 }
 
 trait Tree[+T]{
   def elem: T
   def left:Tree[T]
-  def rigth:Tree[T]
+  def right:Tree[T]
 }
 
 class Branch[+T](val elem:T, val left:Tree[T], val right:Tree[T]) extends Tree[T] {
+
   override def equals(other: Any):Boolean = other match {
     case that: Branch[_] => (that canEqual this ) && this.elem == that.elem && this.left == that.left && this.right == that.right
     case _ => false
