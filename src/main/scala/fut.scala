9c8979350ddaa4edbdc3463b91c825fda25ec9a3
Pietro.Speri <speri.pietro@gmail.com>
2017-11-15 09:41:20 -0800

    Added new writeFile

--- src/main/scala/fut.scala
+++ src/main/scala/fut.scala
@@ -1,4 +1,4 @@
-import java.io.{BufferedWriter, FileWriter}
+import java.io.{BufferedWriter, FileNotFoundException, FileWriter, IOException}
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -11,12 +11,28 @@ object fut {
 
   def writeFile(fileName:String, text:String, overwrite:Boolean): Unit ={
     val bw = new BufferedWriter(new FileWriter(fileName, overwrite))
-    overwrite match {
-      case false => bw.append(text)
-      case true => bw.write(text)
+    try{
+      overwrite match {
+        case false => bw.append(text)
+        case true => bw.write(text)
+      }
+      bw.flush
+      bw.close
+    } catch {
+      case ex:FileNotFoundException => {
+        println("[FILE NOT FOUND EXCEPTION]")
+        ex.printStackTrace
+        ex.getMessage
+        ex.toString
+      }
+      case ex:IOException => {
+        println("[IO EXCEPTION]")
+        ex.printStackTrace
+        ex.getMessage
+        ex.toString
+      }
+      case _ =>
     }
-    bw.flush
-    bw.close
   }
 
 }
