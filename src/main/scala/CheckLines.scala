f9726dfde3638f244a8300a248d72b6d51f556d3
Pietro.Speri <speri.pietro@gmail.com>
2017-12-04 09:51:43 -0800

    fodlLeft via foldRight

--- src/main/scala/CheckLines.scala
+++ src/main/scala/CheckLines.scala
@@ -54,6 +54,10 @@ object CheckLines{
   def foldRightViaFoldLeft[A,B](l: List[A], z: B)(f: (A,B) => B): B =
     foldLeft(reverseList(l), z)((b,a) => f(a,b))
 
+  //FoldLeft via foldRight
+  def foldLeftViaFoldRight[A,B](l: List[A], z: B)(f: (B,A) => B): B =
+    foldRight(l, (b:B) => b)((a,g) => b => g(f(b,a)))(z)
+
   @annotation.tailrec
   def dropWhile[A](l1: List[A], f: A => Boolean): List[A] = l1 match {
     case x :: xs if (f(x)) => dropWhile(xs, f)
