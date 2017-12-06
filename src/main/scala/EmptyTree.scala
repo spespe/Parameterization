d2b758cb12a502607cacceebe350fd56f8f44098
Pietro.Speri <speri.pietro@gmail.com>
2017-12-06 09:31:49 -0800

    Removed previous trait Tree

--- src/main/scala/EmptyTree.scala
+++ src/main/scala/EmptyTree.scala
@@ -13,21 +13,12 @@ object EmptyTree extends Tree[Nothing]{
 
 }
 
-trait Tree[+T]{
+trait Tree1[+T]{
   def elem: T
   def left:Tree[T]
   def right:Tree[T]
 }
 
-class Branch[+T](val elem:T, val left:Tree[T], val right:Tree[T]) extends Tree[T] {
 
-  override def equals(other: Any):Boolean = other match {
-    case that: Branch[_] => (that canEqual this ) && this.elem == that.elem && this.left == that.left && this.right == that.right
-    case _ => false
-  }
 
-  def canEqual(other: Any) = other.isInstanceOf[Branch[_]]
-
-  override def hashCode(): Int = (elem, left, right).##
-}
 
