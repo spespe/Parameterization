3f68a131d39322740dda513a5fd32f3f89120ee5
Pietro.Speri <speri.pietro@gmail.com>
2017-12-07 09:30:42 -0800

    Added recursive implementation of size for Tree

--- src/main/scala/EmptyTree.scala
+++ src/main/scala/EmptyTree.scala
@@ -23,4 +23,8 @@ sealed trait Tree[+A]
 case class Leaf[A](value:A) extends Tree[A]
 case class Branch[A](left:Tree[A], right:Tree[A]) extends Tree[A]
 
-
+//Recursive implementation of size method
+def size[A](t:Tree[A]):Int = t match {
+  case Leaf(_) => 1
+  case Branch(l,r) => 1 + size(l) + size(r)
+}
